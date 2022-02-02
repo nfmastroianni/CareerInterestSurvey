4aed4c9267616fdf2348597d194c567e58f56022
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2022-02-02 13:16:08 -0500

    getting data

--- functions/getForms.js
+++ functions/getForms.js
@@ -1,22 +1,48 @@
-const axios = require("axios");
+const { google } = require("googleapis");
 
 const getForms = async () => {
-  let result = await (async () => {
-    try {
-      const response = axios.get("https://github.com/users/nmastroianni");
-      return response;
-    } catch (error) {
-      console.log("Here comes the axios error...", error);
-    }
-  })();
-  return result;
+  // authorize using the service account
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "./credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  // generate an auth client for use with google sheets api
+  const client = await auth.getClient();
+  const builderId = `${process.env.BUILDER_SPREADSHEET_ID}`;
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  // start pulling data
+  const meta = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId: builderId,
+  });
+  const sheets = meta.data.sheets.map((sheet) => sheet.properties.title);
+  const getData = async () => {
+    return Promise.all(
+      sheets.map(async (sheet) => {
+        const data = await googleSheets.spreadsheets.values.get({
+          auth,
+          spreadsheetId: builderId,
+          range: sheet,
+        });
+        return data;
+      })
+    );
+  };
+  const sheetData = await getData().then((data) => {
+    return data;
+  });
+
+  meta.sheetData = sheetData;
+
+  return meta;
 };
 
 exports.handler = async (event, context) => {
   const formData = await getForms();
-  console.log(JSON.stringify(formData));
 
   return {
-    body: JSON.stringify(formData)
+    statusCode: formData.status,
+    body: JSON.stringify(formData),
   };
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { CircularProgress } from "@mui/material";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const [formData, setFormData] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("http://localhost:8888/api/getForms")
+        .then((response) => response)
+        .then(setFormData);
+    }, 1000);
+  }, []);
+  if (!formData) {
+    return (
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <CircularProgress
+          color="success"
+          style={{ width: "200px", height: "200px" }}
+        />
+      </div>
+    );
+  } else {
+    console.log("formData = ", formData);
+    return (
+      <div className="App">
+        <h1>Data Loaded</h1>
+        <div>{JSON.stringify(formData.data.sheetData)}</div>
+      </div>
+    );
+  }
 }
